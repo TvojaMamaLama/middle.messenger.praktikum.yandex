f0e784e152433b06ae29d1623383a1b35582d7dc
tvojamamalama <aleshnikov9@gmail.com>
2024-03-03 21:29:07 +0300

    сборка + тестовые компоненты

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { Button } from './button';
+export { Link } from './link';
+export { ChatItem } from './chat-item';
+export { ChatList } from './chat-list';
+export { PageTitle } from './page-title';
+export { InputField } from './input-field';
---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import Handlebars from 'handlebars';
+import * as Components from './components';
+import * as Pages from './pages';
+
+const pages: any = {
+  'chat': [Pages.ChatPage],
+  'login': [Pages.LoginPage],
+};
+
+Object.entries(Components).forEach(([ name, component]: any) => {
+  Handlebars.registerPartial(name, component);
+});
+
+function navigate(page: string) {
+  const [source, args] = pages[page];
+  const handlebarsFunct = Handlebars.compile(source);
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML = handlebarsFunct(args);
+};
+
+document.addEventListener("DOMContentLoaded", () => navigate('login'));
+
+document.addEventListener('click', (e: any) => {
+  const page = e.target.getAttribute('page');
+  console.log(page);
+  if (page) {
+    navigate(page);
+
+    e.preventDefault();
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
+});
