25b4af4d2bc710f7c488a2e93d6a0de345549fde
tvojamamalama <aleshnikov9@gmail.com>
2024-03-11 22:39:08 +0300

    last!

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    root: resolve(__dirname, 'src'),
+    root: resolve('src'),
     build: {
-        outDir: resolve(__dirname, 'dist'),
+        outDir: resolve('dist'),
     },
 })
