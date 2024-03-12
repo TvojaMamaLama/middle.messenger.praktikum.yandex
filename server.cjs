1ac1e1adaea22cef34eb81aa321c5140b2db2cbb
tvojamamalama <aleshnikov9@gmail.com>
2024-03-12 15:46:44 +0300

    help

--- server.cjs
+++ server.cjs
@@ -1,19 +1,18 @@
-const express = require("express");
+const express = require('express')
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${__dirname}/dist`))
 
-app.use("/*", (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
-});
+app.use('/*', (req, res) => {
+    res.sendFile(`${__dirname}/dist/index.html`)
+})
 
-app.get("/", (req, res) => {
-  res.status(200);
-  res.send("Suuuuuu!");
-});
+app.get('/', (req, res) => {
+    res.status(200)
+})
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+    console.log(`Example app listening on port ${PORT}!`)
+})
